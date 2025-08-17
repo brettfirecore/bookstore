6b73022f0caeb24787652631a1e133ada161cdd2
brettfirecore <klavian333@gmail.com>
2025-08-17 18:21:16 +1200

    update Tests in  boookstore

--- bookstore.go
+++ bookstore.go
@@ -2,6 +2,8 @@
 // ftl-21.2
 package bookstore
 
+import "fmt"
+
 type Book struct {
 	Title           string
 	Author          string
@@ -9,9 +11,17 @@ type Book struct {
 	ID              int
 	PriceCents      int
 	DiscountPercent int
+	category        string
 }
 
-func (b *Book) SetPriceCents(price int) error {
-	b.PriceCents = price // nope
+func (b *Book) SetCategory(category string) error {
+	if category != "Autobiography" {
+		return fmt.Errorf("unknown category %q", category)
+	}
+	b.category = category
 	return nil
+}
+
+func (b Book) Category() string {
+	return b.category
 }
--- bookstore_test.go
+++ bookstore_test.go
@@ -6,16 +6,33 @@ import (
 	"testing"
 
 	"github.com/brettfirecore/bookstore"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func TestSetPriceCents(t *testing.T) {
+func TestSetCategory(t *testing.T) {
+	t.Parallel()
+
+	b := bookstore.Book{
+		Title: "For the Love of Go",
+	}
+	want := "Autobiography"
+	err := b.SetCategory(want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := b.Category()
+	if want != got {
+		t.Errorf("want category %q, got %q", want, got)
+	}
+}
+
+func TestSetCategoryInvalid(t *testing.T) {
 	t.Parallel()
 	b := bookstore.Book{
-		Title:      "For the Love of Go",
-		PriceCents: 4000,
+		Title: "For the Love of Go",
 	}
-	err := b.SetPriceCents(-1)
-	if err != nil {
-		t.Fatal("wanr error setting invalid price -1, got nil")
+	err := b.SetCategory("bogus")
+	if err == nil {
+		t.Fatal("want error for invalid category, got nil")
 	}
 }
